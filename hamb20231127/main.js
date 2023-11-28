9277e8aa4037932cfe7cdb5e2e04d11b375b8fce
takacskr <takacs.kr@gmail.com>
2023-11-28 18:43:00 +0100

    Added calculated sauces, extra and quantity functions

--- hamb20231127/main.js
+++ hamb20231127/main.js
@@ -1,3 +1,36 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let orderForm = document.getElementById('orderForm');
+document.addEventListener("DOMContentLoaded", () => {
+  let orderForm = document.getElementById("orderForm");
+  let btn = orderForm.querySelector("button.btn.btn-primary");
+
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const price = 2800;
+    let quantityInput = orderForm.querySelector("#quantityInput");
+    let amount = 0;
+
+    if (quantityInput) {
+      amount =
+        (price + getSauces() + getExtra()) * parseInt(quantityInput.value);
+    }
+    console.log(amount);
+  });
+
+  // Get the summary of all selected sauces
+  const getSauces = () => {
+    let sauces = orderForm.querySelectorAll('input[type="checkbox"]:checked');
+    let selectedSauces = 0;
+
+    sauces.forEach((sauce) => {
+      selectedSauces += parseInt(sauce.value);
+    });
+
+    return selectedSauces;
+  };
+
+  // Get the value of the selected extra
+  const getExtra = () => {
+    let extra = orderForm.querySelector('input[name="extra"]:checked');
+    return extra != null ? extra.value : 0;
+  };
 });
