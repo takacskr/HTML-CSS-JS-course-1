bec98ea17d77fc59a1052cd53a634d107196cba4
takacskr <takacs.kr@gmail.com>
2023-11-29 16:00:24 +0100

    With delivery fee

--- hamb20231127/main.js
+++ hamb20231127/main.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   let orderForm = document.getElementById('orderForm');
   let btn = orderForm.querySelector('button.btn.btn-primary');
   let priceOutput = orderForm.querySelector('#priceOutput+label');
+  let isDelivery = orderForm.querySelector('#isDelivery');
 
   btn.addEventListener('click', (e) => {
     e.preventDefault();
@@ -9,16 +10,24 @@ document.addEventListener('DOMContentLoaded', () => {
     const PRICE = 2800;
     let amount = 0;
     let quantity = parseInt(orderForm.querySelector('#quantityInput').value);
+    let delivery = parseInt(isDelivery.value);
 
     if (!isNaN(quantity) && quantity > 0 && quantity < 11) {
       amount = (PRICE + getSauces() + getExtra()) * parseInt(quantity);
-      priceOutput.innerHTML = `Fizetendő: ${amount} Ft.`;
+      console.log(amount);
+      if (isDelivery.checked && amount <= 5000) {
+        amount += delivery;
+      } else {
+        delivery = 0;
+      }
+
+      priceOutput.innerHTML = `Fizetendő: ${amount} Ft. Ebből a szállítási díj: ${delivery}`;
     }
   });
 
   // Get the summary of all selected sauces
   const getSauces = () => {
-    let sauces = orderForm.querySelectorAll('input[type="checkbox"]:checked');
+    let sauces = orderForm.querySelectorAll('input[name="sauce"]:checked');
     let selectedSauces = 0;
 
     sauces.forEach((sauce) => {
