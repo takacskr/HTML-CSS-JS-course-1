73b9cb615e6bb9db451551b245f18d527ffea0ac
takacskr <takacs.kr@gmail.com>
2023-11-29 11:20:50 +0100

    App is ready

--- hamb20231127/main.js
+++ hamb20231127/main.js
@@ -1,23 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
   let orderForm = document.getElementById('orderForm');
   let btn = orderForm.querySelector('button.btn.btn-primary');
+  let priceOutput = orderForm.querySelector('#priceOutput+label');
 
   btn.addEventListener('click', (e) => {
     e.preventDefault();
 
-    const price = 2800;
+    const PRICE = 2800;
     let amount = 0;
-    let quantityInput = orderForm.querySelector('#quantityInput');
+    let quantity = parseInt(orderForm.querySelector('#quantityInput').value);
 
-    if (
-      !isNaN(quantityInput.value) &&
-      quantityInput.value > 0 &&
-      quantityInput.value < 11
-    ) {
-      amount =
-        (price + getSauces() + getExtra()) * parseInt(quantityInput.value);
+    if (!isNaN(quantity) && quantity > 0 && quantity < 11) {
+      amount = (PRICE + getSauces() + getExtra()) * parseInt(quantity);
+      priceOutput.innerHTML = `Fizetendő: ${amount} Ft.`;
     }
-    console.log(amount);
   });
 
   // Get the summary of all selected sauces
