d8566f47e5f2b39526162aed3eb4c6381c830ec6
takacskr <takacs.kr@gmail.com>
2023-12-03 13:32:11 +0100

    Added new algorithms: separated a new JS file

---
+++ hamb20231127/algorithms.js
@@ -0,0 +1,48 @@
+let celsiusArr = ['-2', '42', '17.8', '22.5', '25.8', '32', '10'];
+
+let numericArr = celsiusArr.map((value) => parseFloat(value));
+
+// Using the JavaScript built-in function for get the min number
+const getMinCelsiusDegree = (arr) => {
+    return Math.min(...arr);
+};
+
+// Using the JavaScript built-in function for get the max number
+const getMaxCelsiusDegree = (arr) => {
+    return Math.max(...arr);
+};
+
+// Bubble sort function
+const bubbleShort = (numericArr) => {
+    for (j = 0; j < numericArr.length; j++) {
+        for (let i = 0; i < numericArr.length; i++) {
+            let swap;
+            if (numericArr[i + 1] < numericArr[i]) {
+                swap = numericArr[i];
+                numericArr[i] = numericArr[i + 1];
+                numericArr[i + 1] = swap;
+            }
+        }
+    }
+
+    return numericArr;
+};
+
+const bubbleSortArray = (arr) => {
+    let swap;
+    do {
+        swap = false;
+
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] > arr[i + 1]) {
+                [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+                swap = true;
+            }
+        }
+    } while (swap);
+
+    return arr;
+};
+
+bubbleSortArray(numericArr);
+console.log(numericArr);
--- hamb20231127/offer.js
+++ hamb20231127/offer.js
@@ -3,25 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let numericArr = celsiusArr.map((value) => parseFloat(value));
 
-    const bubbleSortArray = (arr) => {
-        let swap;
-        do {
-            swap = false;
-
-            for (let i = 0; i < arr.length; i++) {
-                if (arr[i] > arr[i + 1]) {
-                    [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
-                    swap = true;
-                }
-            }
-        } while (swap);
-
-        return arr;
-    };
-
-    bubbleSortArray(numericArr);
-    console.log(numericArr);
-
     daySelectInput.addEventListener('change', () => {
         let celsiusOutput = orderForm.querySelector('#celsiusOutput');
         celsiusOutput.textContent = `A várható hőmérséklet: ${getCelsiusDegree()} °C`;
@@ -114,33 +95,3 @@ document.addEventListener('DOMContentLoaded', () => {
         return avg.toFixed(2);
     };
 });
-
-/*
-// Using the JavaScript built-in function for get the min number
-const getMinCelsiusDegree = (arr) => {
-  return Math.min(...arr);
-};
- 
-// Using the JavaScript built-in function for get the max number
-const getMaxCelsiusDegree = (arr) => {
-  return Math.max(...arr);
-};
-*/
-
-/*
-// Bubble sort function
-const bubbleShort = (numericArr) => {
-  for (j = 0; j < numericArr.length; j++) {
-    for (let i = 0; i < numericArr.length; i++) {
-      let swap;
-      if (numericArr[i + 1] < numericArr[i]) {
-        swap = numericArr[i];
-        numericArr[i] = numericArr[i + 1];
-        numericArr[i + 1] = swap;
-      }
-    }
-  }
-
-  return numericArr;
-};
-*/
