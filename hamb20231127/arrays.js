5722426cf171f53f8961710785da5cd614968785
takacs.kr <takacs.kr@gmail.com>
2023-12-05 18:12:53 +0100

    Playing with Object data

--- hamb20231127/arrays.js
+++ hamb20231127/arrays.js
@@ -6,22 +6,49 @@ console.log(Array.isArray(arr));
 
 // for in ciklus a KULCSOKON, IDEXEKEN meg végig
 for (let key in arr) {
-	console.log('for in ciklus', key);
+    console.log('for in ciklus', key);
 };
 
 // for of ciklus az ÉRTÉKEKEN megy végig, CSAK TÖMBÖT TUD BEJÁRNI
 for (let value of arr) {
-	console.log('for of ciklus', value);
+    console.log('for of ciklus', value);
 };
 
 let fruits = arr.splice(1, 3, "lemon", "kiwi");
 */
 
 let user = {
-	name: "Kiss Ramóna",
-	age: 20,
-	city: "Bp"
+    name: "Kiss Ramóna",
+    age: 20,
+    city: "Bp"
 };
+
+
+
+data = {
+    "weathers": [
+        {"dayNumber": 0, "temperature": 11.2},
+        {"dayNumber": 1, "temperature": 15.5},
+        {"dayNumber": 2, "temperature": 18.9},
+        {"dayNumber": 3, "temperature": 9.0},
+        {"dayNumber": 4, "temperature": 14.7},
+        {"dayNumber": 5, "temperature": 20.3},
+        {"dayNumber": 6, "temperature": 22.8},
+    ],
+    "offers": [
+        {"upperLimit": 0, "offerMessage": "Ma forró csokit árusítunk"},
+        {"upperLimit": 15, "offerMessage": "15 fok felett ingyen fagylalt"},
+        {"upperLimit": 10, "offerMessage": "10 fok alatt meleg teát kínálunk"},
+        {"upperLimit": 18, "offerMessage": "18 fokig 20% kedvezmény minden hűsítőre"},
+        {"upperLimit": 22, "offerMessage": "22 fokig strandtörölköző akció"},
+        {"upperLimit": 25, "offerMessage": "25 fok felett jegeskávé dupla adaggal"},
+        {"upperLimit": 26, "offerMessage": "26 fok felett új, frissítő smoothieink vannak"},
+    ]
+}
+
+for (let value of Object.values(data)) {
+    console.log(value[6].temperature);
+}
 
 /*
 let admin = new Object();
@@ -52,21 +79,22 @@ console.log(Object.keys(user).length);
 
 // Objektum bejárása for in ciklussal
 for (let k in user) {
-	console.log('Key: ', k, 'Value: ', user[k]);
+    console.log('Key: ', k, 'Value: ', user[k]);
 }
 
 // Objektum bejárása for of ciklussal. CSAK TÖMBÖT TUD BEJÁRNI,
 // kiolvasom a kulcsokat: az Object.keys(user) az egy már egy TÖMB!
 for (let k of Object.keys(user)) {
-	console.log(user[k]);
+    console.log(user[k]);
 }
 
 // ÉRTÉKEK kiolvasása az objektumból, egy TÖMBBEN adja vissza
 for (let value of Object.values(user)) {
-	console.log(value);
+    console.log(value);
+}
+
+// a ciklus végigmegy a KULCS - ÉRTÉK párokon
+for (let entry of Object.entries(user)) {
+    console.log('entries: ', entry[0], entry[1]);
 }
 */
-
-for (let entry of Object.entries(user)) {
-	console.log('entries: ', entry[0], entry[1]);
-}
