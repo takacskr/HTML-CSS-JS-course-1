3775f586ca0c913b581910693d93d7617551fd72
takacs.kr <takacs.kr@gmail.com>
2023-12-06 14:56:43 +0100

    Refactoring is ready

--- hamb20231127/arrays.js
+++ hamb20231127/arrays.js
@@ -17,50 +17,83 @@ for (let value of arr) {
 let fruits = arr.splice(1, 3, "lemon", "kiwi");
 */
 
-let user = {
+/* let user = {
     name: 'Kiss Ramóna',
     age: 20,
     city: 'Bp',
-};
+}; */
 
 data = {
-    weathers: [
-        { dayNumber: 0, temperature: 11.2 },
-        { dayNumber: 1, temperature: 15.5 },
-        { dayNumber: 2, temperature: 18.9 },
-        { dayNumber: 3, temperature: 9.0 },
-        { dayNumber: 4, temperature: 14.7 },
-        { dayNumber: 5, temperature: 20.3 },
-        { dayNumber: 6, temperature: 22.8 },
-    ],
-    offers: [
-        { upperLimit: 0, offerMessage: 'Ma forró csokit árusítunk' },
-        { upperLimit: 15, offerMessage: '15 fok felett ingyen fagylalt' },
-        { upperLimit: 10, offerMessage: '10 fok alatt meleg teát kínálunk' },
-        {
-            upperLimit: 18,
-            offerMessage: '18 fokig 20% kedvezmény minden hűsítőre',
-        },
-        { upperLimit: 22, offerMessage: '22 fokig strandtörölköző akció' },
-        {
-            upperLimit: 25,
-            offerMessage: '25 fok felett jegeskávé dupla adaggal',
-        },
-        {
-            upperLimit: 26,
-            offerMessage: '26 fok felett új, frissítő smoothieink vannak',
-        },
-    ],
+    weathers:
+            [
+                { dayNumber: 0, temperature: 11.2 },
+                { dayNumber: 1, temperature: 15.5 },
+                { dayNumber: 2, temperature: 18.9 },
+                { dayNumber: 3, temperature: 9.0 },
+                { dayNumber: 4, temperature: 14.7 },
+                { dayNumber: 5, temperature: 20.3 },
+                { dayNumber: 6, temperature: 22.8 },
+            ],
+    
+    offers:
+            [
+                { upperLimit: 0, offerMessage: 'Ma forró csokit árusítunk' },
+                { upperLimit: 15, offerMessage: '15 fok felett ingyen fagylalt' },
+                { upperLimit: 10, offerMessage: '10 fok alatt meleg teát kínálunk' },
+                { upperLimit: 18, offerMessage: '18 fokig 20% kedvezmény minden hűsítőre'},
+                { upperLimit: 22, offerMessage: '22 fokig strandtörölköző akció' },
+                { upperLimit: 25, offerMessage: '25 fok felett jegeskávé dupla adaggal'},
+                { upperLimit: 26, offerMessage: '26 fok felett új, frissítő smoothieink vannak'}
+            ]
 };
 
-/* for (let i = 0; i < data.weathers.length; i++) {
+for (const offer of data.offers) {
+    console.log(offer.offerMessage);
+}
+
+
+function getOfferForTemperature(temperature) {
+    // Find the offer for the given temperature
+    for (const offer of data.offers) {
+      if (temperature <= offer.upperLimit) {
+        return offer.offerMessage;
+      }
+    }
+  
+    // If no offer is found, provide a default message
+    return 'Nincs ajánlat a megadott hőmérsékletre';
+  }
+  
+  // Example: Get the offer for a temperature of 18.5
+  const temperature = 18.5;
+  const offer = getOfferForTemperature(temperature);
+  
+  console.log(`For a temperature of ${temperature}°C, the offer is: ${offer}`);
+
+
+/*
+console.log(Object.entries(data)[0][1][0].temperature);
+
+// A data objektum kulcsait adja vissza: weathers, offers
+for (let i = 0; i < Object.values(data).length; i++) {
+    console.log(Object.keys(data)[i]);
+}
+
+// A data objektumban lévő első kulcsban található objektum i-edik elemének temperature kulcs értékével tér vissza
+for (i = 0; i < Object.values(data)[0].length; i++) {
+    console.log(Object.values(data)[0][i].temperature);
+}
+
+console.log(Object.values(data).length);
+
+for (let i = 0; i < data.weathers.length; i++) {
     console.log(data.weathers[i].temperature);
     console.log(data.offers[i].offerMessage);
-} */
-/* 
+}
+
 for (entry of Object.entries(data)) {
     console.log(entry[1][1]);
-} */
+}
 
 let weathers = Object.entries(data)[0][1];
 
@@ -68,7 +101,6 @@ for (let i = 0; i < weathers.length; i++) {
     console.log(weathers[i]['temperature']);
 }
 
-/*
 let admin = new Object();
 
 let customer = {};
