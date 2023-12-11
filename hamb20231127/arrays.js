04456dac910440242be7627be9f37dca91984d62
takacskr <takacs.kr@gmail.com>
2023-12-11 20:01:47 +0100

    Modified arrays

--- hamb20231127/arrays.js
+++ hamb20231127/arrays.js
@@ -154,6 +154,10 @@ for (let entry of Object.entries(user)) {
 }
 */
 
+// The purpose of "use strict" is to indicate that the code should be executed in "strict mode".
+'use strict';
+
+// forEach()
 // This method can help you to loop over array’s items.
 const arr1 = [1, 2, 3, 4, 5, 6];
 
@@ -161,21 +165,23 @@ arr1.forEach((item) => {
     console.log(item); // output: 1 2 3 4 5 6
 });
 
+// includes()
 // This method check if array includes the item passed in the method.
 const arr2 = [1, 2, 3, 4, 5, 6];
 
-arr2.includes(2); // output: true
-arr2.includes(7); // output: false
+console.log(arr2.includes(2)); // output: true
+console.log(arr2.includes(7)); // output: false
 
+// filter()
 // This method create new array with only elements passed condition inside the provided function.
 const arr3 = [1, 2, 3, 4, 5, 6];
 
-// item(s) greater than 3
 const filtered = arr3.filter((num) => num > 3);
 console.log(filtered); // output: [4, 5, 6]
 
 console.log(arr3); // output: [1, 2, 3, 4, 5, 6]
 
+// map()
 // This method create new array by calling the provided function in every element.
 const arr4 = [1, 2, 3, 4, 5, 6];
 
@@ -184,3 +190,84 @@ const oneAdded = arr4.map((num) => num + 1);
 console.log(oneAdded); // output [2, 3, 4, 5, 6, 7]
 
 console.log(arr4); // output: [1, 2, 3, 4, 5, 6]
+
+// reduce()
+// The reduce() method applies a function against an accumulator and each element
+// in the array (from left to right) to reduce it to a single value
+const arr5 = [1, 2, 3, 4, 5, 6];
+
+const sum = arr5.reduce((total, value) => total + value, 0);
+console.log(sum); // 21
+
+// some()
+// This method check if at least one of array’s item passed the condition.
+// If passed, it return ‘true’ otherwise ‘false’.
+const arr6 = [1, 2, 3, 4, 5, 6];
+
+// at least one element is greater than 4?
+const largeNum = arr6.some((num) => num > 4);
+console.log(largeNum); // output: true
+
+// at least one element is less than or equal to 0?
+const smallNum = arr6.some((num) => num <= 0);
+console.log(smallNum); // output: false
+
+// every()
+// This method check if all array’s item passed the condition.
+// If passed, it return ‘true’ otherwise ‘false’.
+const arr7 = [1, 2, 3, 4, 5, 6];
+
+// all elements are greater than 4
+const greaterFour = arr7.every((num) => num > 4);
+console.log(greaterFour); // output: false
+
+// all elements are less than 10
+const lessTen = arr7.every((num) => num < 10);
+console.log(lessTen); // output: true
+
+// sort()
+// This method used to arrange/sort array’s item either ascending or descending order.
+const arr8 = [1, 2, 3, 4, 5, 6];
+const alpha = ['e', 'a', 'c', 'u', 'y'];
+
+// sort in descending order
+let descOrder = arr8.sort((a, b) => (a > b ? -1 : 1));
+console.log(descOrder); // output: [6, 5, 4, 3, 2, 1]
+
+// sort in ascending order
+let ascOrder = alpha.sort((a, b) => (a > b ? 1 : -1));
+console.log(ascOrder); // output: ['a', 'c', 'e', 'u', 'y']
+
+// Array.from()
+// This change all thing that are array-like or iterable into true array especially
+// when working with DOM, so that you can use other array methods like reduce, map, filter and so on.
+const n = 'frugence';
+const nArray = Array.from(n);
+
+console.log(n); // output: frugence
+console.log(nArray); // output: ['f', 'r', 'u', 'g', 'e', 'n', 'c', 'e']
+
+const lis = document.querySelectorAll('li');
+const lisArray = Array.from(document.querySelectorAll('li'));
+console.log(lisArray);
+
+// is true array?
+console.log(Array.isArray(lis)); // output: false
+console.log(Array.isArray(lisArray)); // output: true
+
+// Array.of()
+// This create array from every arguments passed into it.
+const nums = Array.of(1, 2, 3, 4, 5, 6);
+console.log(nums); // output: [1, 2, 3, 4, 5, 6]
+
+const balance = 1463879;
+
+// Format the number as a currency
+const formattedBalance = balance.toLocaleString('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+});
+
+console.log(formattedBalance); // Output: $1,463,879
