27f7b33ae4d8de3cafaea067304d8bb64ea9ca85
takacskr <takacs.kr@gmail.com>
2023-12-27 16:04:10 +0100

    Added the data modify function

--- hamb20231127/main.js
+++ hamb20231127/main.js
@@ -1,66 +1,110 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // Get references to form elements
-  let orderForm = document.getElementById('orderForm');
-  let btn = orderForm.querySelector('button.btn.btn-primary');
-  let priceOutput = orderForm.querySelector('#priceOutput+label');
-  let isDeliveryInput = orderForm.querySelector('#isDeliveryInput');
+    // Get references to form elements
+    let orderForm = document.getElementById('orderForm');
+    let btn = orderForm.querySelector('button.btn.btn-primary');
+    let priceOutput = orderForm.querySelector('#priceOutput+label');
+    let isDeliveryInput = orderForm.querySelector('#isDeliveryInput');
 
-  // Add click event listener to the button
-  btn.addEventListener('click', (e) => {
-    e.preventDefault();
+    let _ = 0;
 
-    const PRICE = 2800;
-    let amount = 0;
-    let quantity = parseInt(orderForm.querySelector('#quantityInput').value);
+    /*
+    // Add click event listener to the button
+    btn.addEventListener('click', (e) => {
+        e.preventDefault();
 
-    // Check if quantity is valid
-    if (!isNaN(quantity) && quantity > 0 && quantity < 11 && isValidAddress()) {
-      // Calculate the total amount including sauces, extras, and quantity
-      amount = (PRICE + getSauces() + getExtra()) * parseInt(quantity);
+        const PRICE = 2800;
+        let amount = 0;
+        let quantity = parseInt(
+            orderForm.querySelector('#quantityInput').value
+        );
 
-      // Use the arrow function to get the delivery amount
-      let delivery = isDelivery(amount);
+        // Check if quantity is valid
+        if (
+            !isNaN(quantity) &&
+            quantity > 0 &&
+            quantity < 11 &&
+            isValidAddress()
+        ) {
+            // Calculate the total amount including sauces, extras, and quantity
+            amount = (PRICE + getSauces() + getExtra()) * parseInt(quantity);
 
-      // Display the total amount and delivery charge
-      priceOutput.innerHTML = `Fizetendő: ${amount} Ft. Ebből a szállítási díj: ${delivery}`;
-    }
-  });
+            // Use the arrow function to get the delivery amount
+            let delivery = isDelivery(amount);
 
-  // Function to get the total value of selected sauces
-  const getSauces = () => {
-    let sauces = orderForm.querySelectorAll('input[name="sauce"]:checked');
-    let selectedSauces = 0;
-
-    sauces.forEach((sauce) => {
-      selectedSauces += parseInt(sauce.value);
+            // Display the total amount and delivery charge
+            priceOutput.innerHTML = `Fizetendő: ${amount} Ft. Ebből a szállítási díj: ${delivery}`;
+        }
     });
+    */
 
-    return selectedSauces;
-  };
+    // Function to get the total value of selected sauces
+    const getSauces = () => {
+        let sauces = orderForm.querySelectorAll('input[name="sauce"]:checked');
+        let selectedSauces = 0;
 
-  // Function to get the value of the selected extra
-  const getExtra = () => {
-    let extra = orderForm.querySelector('input[name="extra"]:checked');
-    return extra != null ? parseInt(extra.value) : 0;
-  };
+        sauces.forEach((sauce) => {
+            selectedSauces += parseInt(sauce.value);
+        });
 
-  // Function to determine delivery amount
-  const isDelivery = (amount) => {
-    let delivery = parseInt(isDeliveryInput.value);
-    return isDeliveryInput.checked && amount <= 5000 ? delivery : 0;
-  };
+        return selectedSauces;
+    };
 
-  // Function to check whether the address is valid
-const isValidAddress = () => {
-  const addressInput = orderForm.querySelector('#addressInput');
-  
-  if (addressInput.value.length < 10) {
-    alert('A cím nem lehet 10 karakternél rövidebb!');
-    addressInput.focus();
-    return false;
-  }
+    // Function to get the value of the selected extra
+    const getExtra = () => {
+        let extra = orderForm.querySelector('input[name="extra"]:checked');
+        return extra != null ? parseInt(extra.value) : 0;
+    };
 
-  return true;
-}
+    // Function to determine delivery amount
+    const isDelivery = (amount) => {
+        let delivery = parseInt(isDeliveryInput.value);
+        return isDeliveryInput.checked && amount <= 5000 ? delivery : 0;
+    };
 
+    // Function to check whether the address is valid
+    const isValidAddress = () => {
+        const addressInput = orderForm.querySelector('#addressInput');
+
+        if (addressInput.value.length < 10) {
+            alert('A cím nem lehet 10 karakternél rövidebb!');
+            addressInput.focus();
+            return false;
+        }
+
+        return true;
+    };
+
+    function modifyElements(selector, attribute, value) {
+        let e = document.querySelectorAll(selector);
+        for (let i = 0; i < e.length; i++) {
+            if (attribute.startsWith('style.')) {
+                styleProperty = attribute.split('.')[1];
+                e[i].style[styleProperty] = value;
+            } else {
+                e[i][attribute] = value;
+            }
+        }
+    }
+
+    modifyElements('input', 'style.backgroundColor', 'rgba(255, 145, 123, .5)');
+
+    // Add element to the markup
+    let chooseSauce = document.createElement('small');
+    chooseSauce.className = 'form-text text-muted';
+    chooseSauce.innerHTML = 'Válasszon szószt!';
+
+    let parent = document.querySelector('#orderForm > div:nth-child(2)');
+    parent.appendChild(chooseSauce);
+
+    orderForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+        console.log(this);
+
+        let inputs = this.querySelectorAll('input');
+        let values = {};
+        for (let i = 0; i < inputs.length; i++) {
+            values[inputs[i].name] = inputs[i].value;
+        }
+        console.log(values);
+    });
 });
--- table/main.js
+++ table/main.js
@@ -1,101 +1,136 @@
 document.addEventListener('DOMContentLoaded', () => {
-  'use strict';
+    'use strict';
 
-  const tableApp = {
-    data: {
-      users: [
-        { id: '3dr4', name: 'Kiss Béla', email: 'bela@printcenter.hu' },
+    const tableApp = {
+        data: {
+            users: [
+                { id: '3dr4', name: 'Kiss Béla', email: 'bela@printcenter.hu' },
 
-        { id: '4t68', name: 'Nagy Zoltán', email: 'zoltan.nagy@gmail.com' },
-        { id: 's3gz', name: 'Horváth János', email: 'hjanos@pvckft.hu' },
-        { id: '87ji', name: 'Szép Judit', email: 'juditszep@alfaholding.com' },
-        { id: '7iok', name: 'Budavári Kálmán', email: 'b.kalman@gmail.com' },
-        { id: 'swaf', name: 'Seszták Szabolcs', email: 'szabis@galinfo.hu' },
-        {
-          id: '5rgz',
-          name: 'Hédervári Henrietta',
-          email: 'h.heni@gkesekollok.com',
+                {
+                    id: '4t68',
+                    name: 'Nagy Zoltán',
+                    email: 'zoltan.nagy@gmail.com',
+                },
+                {
+                    id: 's3gz',
+                    name: 'Horváth János',
+                    email: 'hjanos@pvckft.hu',
+                },
+                {
+                    id: '87ji',
+                    name: 'Szép Judit',
+                    email: 'juditszep@alfaholding.com',
+                },
+                {
+                    id: '7iok',
+                    name: 'Budavári Kálmán',
+                    email: 'b.kalman@gmail.com',
+                },
+                {
+                    id: 'swaf',
+                    name: 'Seszták Szabolcs',
+                    email: 'szabis@galinfo.hu',
+                },
+                {
+                    id: '5rgz',
+                    name: 'Hédervári Henrietta',
+                    email: 'h.heni@gkesekollok.com',
+                },
+                {
+                    id: 'bhu6',
+                    name: 'László Károly',
+                    email: 'karolylaci@vendettaemail.hu',
+                },
+            ],
         },
-        {
-          id: 'bhu6',
-          name: 'László Károly',
-          email: 'karolylaci@vendettaemail.hu',
+
+        // Function to create a button with an icon
+        createIconButton: function (iconClass, buttonClass, clickHandler) {
+            const button = document.createElement('button');
+            button.classList.add('btn', buttonClass);
+            button.style.margin = '5px';
+
+            const icon = document.createElement('i');
+            icon.classList.add('bi', iconClass);
+            button.appendChild(icon);
+
+            // Add a click event listener to the button
+            button.addEventListener('click', clickHandler);
+
+            return button;
         },
-      ],
-    },
 
-    // Function to create a button with an icon
-    createIconButton: function (iconClass, buttonClass, clickHandler) {
-      const button = document.createElement('button');
-      button.classList.add('btn', buttonClass);
-      button.style.margin = '5px';
+        modifyData: function (elements) {
+            for (let i = 0; i < elements.length; i++) {
+                const textContent = elements[i].textContent.trim();
+                if (textContent !== '') {
+                    const textBox = document.createElement('input');
+                    textBox.type = 'text';
+                    textBox.value = textContent;
 
-      const icon = document.createElement('i');
-      icon.classList.add('bi', iconClass);
-      button.appendChild(icon);
+                    elements[i].textContent = '';
+                    elements[i].appendChild(textBox);
+                }
+            }
+        },
 
-      // Add a click event listener to the button
-      button.addEventListener('click', clickHandler);
+        // Function to create a delete button
+        createDeleteButton: function () {
+            return this.createIconButton('bi-trash', 'btn-danger', function () {
+                this.parentElement.parentElement.remove();
+            });
+        },
 
-      return button;
-    },
+        createModifyButton: function () {
+            return this.createIconButton(
+                'bi-pencil-square',
+                'btn-success',
+                function () {
+                    let childElements =
+                        this.parentElement.parentElement.children;
+                    tableApp.modifyData(childElements);
+                }
+            );
+        },
 
-    // Function to create a delete button
-    createDeleteButton: function () {
-      return this.createIconButton('bi-trash', 'btn-danger', function () {
-        this.parentElement.parentElement.remove();
-      });
-    },
+        // Function to create a cell for user data
+        createCell: function (data) {
+            const cell = document.createElement('td');
+            cell.textContent = data;
+            return cell;
+        },
 
-    // Function to create a modify button
-    createModifyButton: function () {
-      return this.createIconButton(
-        'bi-pencil-square',
-        'btn-success',
-        function () {
-          console.log('Modify button clicked');
-        }
-      );
-    },
+        // Function to generate a table row for a user
+        generateTableRow: function (user) {
+            const row = document.createElement('tr');
+            const td = document.createElement('td');
 
-    // Function to create a cell for user data
-    createCell: function (data) {
-      const cell = document.createElement('td');
-      cell.textContent = data;
-      return cell;
-    },
+            for (const key in user) {
+                if (user.hasOwnProperty(key)) {
+                    row.appendChild(this.createCell(user[key]));
+                }
+            }
 
-    // Function to generate a table row for a user
-    generateTableRow: function (user) {
-      const row = document.createElement('tr');
-      const td = document.createElement('td');
+            td.appendChild(this.createDeleteButton());
+            row.appendChild(td);
+            td.appendChild(this.createModifyButton());
+            row.appendChild(td);
 
-      for (const key in user) {
-        if (user.hasOwnProperty(key)) {
-          row.appendChild(this.createCell(user[key]));
-        }
-      }
+            return row;
+        },
 
-      td.appendChild(this.createDeleteButton());
-      row.appendChild(td);
-      td.appendChild(this.createModifyButton());
-      row.appendChild(td);
+        // Main function to generate the table
+        generateTable: function () {
+            const tableBody = document.querySelector('.table tbody');
 
-      return row;
-    },
+            for (let i = 0; i < this.data.users.length; i++) {
+                const user = this.data.users[i];
+                const row = this.generateTableRow(user);
+                tableBody.appendChild(row);
+            }
+        },
+    };
 
-    // Main function to generate the table
-    generateTable: function () {
-      const tableBody = document.querySelector('.table tbody');
-
-      for (let i = 0; i < this.data.users.length; i++) {
-        const user = this.data.users[i];
-        const row = this.generateTableRow(user);
-        tableBody.appendChild(row);
-      }
-    },
-  };
-
-  // Call the main function
-  tableApp.generateTable();
+    // Call the main function
+    tableApp.generateTable();
 });
